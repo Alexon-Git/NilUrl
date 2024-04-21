471b4858043a816e30b47f65e8b73eb63f809ba9
rik51 <animeboy3228@outlook.com>
2024-04-22 00:38:46 +0700

    Попытка поставить на pages

--- src/LogicComp/AppRouter.tsx
+++ src/LogicComp/AppRouter.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Route, Routes, useNavigate} from "react-router-dom";
 import {publicRoutes, routerType} from "./routes";
 const AppRouter = () => {
+    const navigate = useNavigate()
+    useEffect(()=>{
+        navigate('Main')
+    },[])
     return (
         <Routes>
             {publicRoutes.map(({path,title,element}:routerType)=>
--- src/components/MainPage/LinkPageMainPart.tsx
+++ src/components/MainPage/LinkPageMainPart.tsx
@@ -4,6 +4,7 @@ import "../../styles/MainPage/LinkPageMainPart.css"
 import Filter from "../LinksPage/Filter";
 import LinksMapNew from "../LinksPage/LinksMapNew";
 import SortButton from "../LinksPage/SortButton";
+import CreateLinkNew from "../Global/CreateLinkNew";
 
 const LinkPageMainPart = () => {
     const [flag, setFlag] = useState(true);
@@ -11,15 +12,19 @@ const LinkPageMainPart = () => {
 
     },[])
     return (
+
         <div className="LinkPageMainPart" >
             <div className="TopContainer">
-                <div className="Top">
+                <div className="FakeDivLP">
+                </div>
+                <div className="RightTopCont">
                     <SortButton/>
+                    <CreateLinkNew/>
                 </div>
             </div>
             <div className="MainContainer">
                 <div className="FilerContainer">
-
+                    <Filter/>
                 </div>
                 <div className="LinksContainer">
                         <LinksMapNew Data={"24/03/24"} clicks={3} pathS={"nil-url.ru/Ffv3cv"} pathL={"https://www.svgrepo.com/collection/dazzle-line-iconsm/collectionqqqqqqqqqqqqqqqqqqqqq"} SvgPath={"/test.svg"} Android={true} IOS={true} UTM={true}/>
