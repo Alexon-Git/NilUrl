59ed96854f2754482d5b6ea62044f1c4102c28e1
rik51 <animeboy3228@outlook.com>
2024-05-02 22:06:41 +0700

    fix URL

--- src/LogicComp/AppRouter.tsx
+++ src/LogicComp/AppRouter.tsx
@@ -1,13 +1,16 @@
 import React, {useEffect} from 'react';
 import {Route, Routes, useNavigate} from "react-router-dom";
 import {publicRoutes, routerType} from "./routes";
+import {MAINPAGE_ROUTE} from "./utils/Const";
+import MainPage from "../pages/MainPage";
 const AppRouter = () => {
     const navigate = useNavigate()
-    useEffect(()=>{
-        navigate('Main')
-    },[])
+    // useEffect(()=>{
+    //     navigate('Main')
+    // },[])
     return (
         <Routes>
+            <Route index key={"mainPage"} path={MAINPAGE_ROUTE}  element={<MainPage/>}/>
             {publicRoutes.map(({path,title,element}:routerType)=>
                 <Route key={title} path={path}  element={element}/>
             )};
