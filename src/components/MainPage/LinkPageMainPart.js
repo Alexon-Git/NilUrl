0d8501176d8d7680b756ff50ac51f748714e09d4
TIvers <wlad2000vita@gmail.com>
2024-06-07 21:25:05 +0700

    Исправление бэка

--- src/components/MainPage/LinkPageMainPart.js
+++ src/components/MainPage/LinkPageMainPart.js
@@ -7,7 +7,7 @@ import CreateLinkNew from "../Global/CreateLinkNew";
 import SortNew from "../LinksPage/SortNew";
 import Cookies from 'js-cookie';
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode }from 'jwt-decode';
 import TagsColumn from "../LinksPage/TagsColumn";
 
 const LinkPageMainPart = () => {
@@ -15,6 +15,7 @@ const LinkPageMainPart = () => {
   const [userStatus, setUserStatus] = useState(null);
   const [reloadKey, setReloadKey] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [selectedTags, setSelectedTags] = useState([]); // Добавлено состояние для выбранных тегов
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -65,7 +66,6 @@ const LinkPageMainPart = () => {
       const doc = parser.parseFromString(html, 'text/html');
       let favicon = '/NilLogo.svg'; 
 
-    
       const iconLink =  doc.querySelector('link[rel="icon"]') ||
                         doc.querySelector('link[rel="shortcut icon"]') ||
                         doc.querySelector('link[rel*="icon"]') ||
@@ -74,14 +74,12 @@ const LinkPageMainPart = () => {
       if (iconLink) {
         favicon = iconLink.href;
       } else {
-       
         const response = await axios.get(proxyUrl + baseUrl + '/favicon.ico');
         if (response.status === 200) {
           favicon = baseUrl + '/favicon.ico';
         }
       }
 
-   
       if (favicon && !favicon.startsWith('http')) {
         favicon = baseUrl + favicon;
       }
@@ -92,17 +90,18 @@ const LinkPageMainPart = () => {
       return '/NilLogo.svg'; 
     }
   };
+
   useEffect(() => {
-   
-  
+    // Используйте reloadKey для повторного рендеринга при изменении состояния
   }, [reloadKey]);
-const convertDateFormat = (dateString) => {
-  const parts = dateString.split('.');
-  return new Date(parts[2], parts[1] - 1, parts[0]);
-};
+
+  const convertDateFormat = (dateString) => {
+    const parts = dateString.split('.');
+    return new Date(parts[2], parts[1] - 1, parts[0]);
+  };
+
   const sortLinks = (option) => {
     let sortedLinks = [...links];
-    console.log(option);
     switch (option) {
       case 0:
         sortedLinks.sort((a, b) => new Date(convertDateFormat(a.Data)) - new Date(convertDateFormat(b.Data)));
@@ -117,57 +116,74 @@ const convertDateFormat = (dateString) => {
         sortedLinks.sort((a, b) => b.pathS.localeCompare(a.pathS));
         break;
       default:
-        
         break;
     }
-    
     setLinks(sortedLinks);
-    setReloadKey(prevKey => prevKey + 1); 
+    setReloadKey(prevKey => prevKey + 1);
   };
+
   const highestKey = links.length > 0 ? Math.max(...links.map(link => link.key)) : 0;
+
+  const updateSelectedTags = (tag) => {
+    setSelectedTags((prevTags) => {
+      if (prevTags.some(t => t.name === tag.name && t.svgColor === tag.svgColor && t.backgrounds === tag.backgrounds)) {
+        return prevTags.filter(t => !(t.name === tag.name && t.svgColor === tag.svgColor && t.backgrounds === tag.backgrounds));
+      } else {
+        return [...prevTags, tag];
+      }
+    });
+  };
+
+  const filteredLinks = links.filter(link => {
+    return selectedTags.every(tag => 
+      link.tagValue !== tag.name || 
+      link.svgColor !== tag.svgColor || 
+      link.backgrounds !== tag.backgrounds
+    );
+  });
 
   return (
     <div className='image-background'>
-    <div className="LinkPageMainPart">
-      <div className="TopContainer">
-        <div className="FakeDivLP"></div>
-        <div className="RightTopCont">
-          <SortNew sortLinks={sortLinks} />
-          <TagsColumn/>
-          <CreateLinkNew userStatus={userStatus} highestKey={highestKey} /> 
+      <div className="LinkPageMainPart">
+        <div className="TopContainer">
+          <div className="FakeDivLP"></div>
+          <div className="RightTopCont">
+            <SortNew sortLinks={sortLinks} />
+            <TagsColumn updateSelectedTags={updateSelectedTags} links={links} /> 
+            <CreateLinkNew userStatus={userStatus} highestKey={highestKey} /> 
+          </div>
+        </div>
+        <div className="MainContainer">
+          <div className="LinksContainer">
+            {filteredLinks.length > 0 ? (
+              filteredLinks.map((link, index) => (
+                <LinksMapNew
+                  key={link.key}
+                  Data={link.Data}
+                  SvgPath={link.SvgPath}
+                  pathS={link.pathS}
+                  pathL={link.pathL}
+                  UTM={link.UTM}
+                  Android={link.Android}
+                  IOS={link.IOS}
+                  clicks={link.clicks}
+                  svgColor={link.svgColor}
+                  backgrounds={link.backgrounds}
+                  tagValue={link.tagValue}
+                  timer_flag={link.timer_flag}
+                  tag_flag={link.tag_flag}
+                />
+              ))
+            ) : (
+              loading ? (
+                <div>Загрузка...</div>
+              ) : (
+                <div>Ссылки не найдены. Пожалуйста, перезагрузите страницу.</div>
+              )
+            )}
+          </div>
         </div>
       </div>
-      <div className="MainContainer">
-  <div className="LinksContainer">
-    {links.length > 0 ? (
-      links.map((link, index) => (
-        <LinksMapNew
-          key={link.key} 
-          Data={link.Data}
-          SvgPath={link.SvgPath}
-          pathS={link.pathS}
-          pathL={link.pathL}
-          UTM={link.UTM}
-          Android={link.Android}
-          IOS={link.IOS}
-          clicks={link.clicks}
-          svgColor={link.svgColor}
-          backgrounds={link.backgrounds}
-          tagValue={link.tagValue}
-          timer_flag={link.timer_flag}
-          tag_flag={link.tag_flag}
-        />
-      ))
-    ) : (
-      loading ? (
-        <div>Загрузка...</div>
-      ) : (
-        <div>Ссылки не найдены. Пожалуйста, перезагрузите страницу.</div>
-      )
-    )}
-  </div>
-</div>
-    </div>
     </div>
   );
 };
