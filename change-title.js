47ac03013008ccbe6d8460162a35876fa03724db
TIvers <wlad2000vita@gmail.com>
2024-06-17 17:55:27 +0700

    Изменение текста index.html

--- change-title.js
+++ change-title.js
@@ -8,13 +8,28 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     console.error(err);
     return;
   }
-  const result = data.replace(/<title>.*<\/title>/, '<title>NilUrl</title>');
 
-  fs.writeFile(filePath, result, 'utf8', (err) => {
+  // Заменяем <title> и добавляем мета-теги
+  const updatedContent = data.replace(
+    /<title>.*<\/title>/,
+    `<title>NilUrl - Аналитика и сокращение ваших ссылок</title>
+    <meta name="description" content="NilUrl предоставляет мощную аналитику ваших ссылок, включая информацию о геолокации, устройстве, браузере и реферере." />
+    <meta name="keywords" content="анализ ссылок, сокращение ссылок, геолокация, устройство, браузер, реферер" />
+    <meta name="yandex-verification" content="69602e98dddc0155" />
+    <script>
+        window.YandexRotorSettings = {
+            WaiterEnabled: true,
+            FailOnTimeout: false,
+            NoJsRedirectsToMain: true
+        }
+    </script>`
+  );
+
+  fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log('Title has been updated to NilUrl');
+    console.log('Title and meta tags have been updated in index.html');
   });
 });
