813d7b0da40cffb83345ad057c940fe986ec20af
Ilya Tetyakov <tetyakov2@gmail.com>
2024-07-22 14:39:38 +0700

    Заменил дизайн верификации

--- src/components/popups/VerifyCodeModal.js
+++ src/components/popups/VerifyCodeModal.js
@@ -71,22 +71,6 @@ const VerifyCodeModal = ({ onClose, onSuccess }) => {
   return (
     <div className="modal-verify">
       <div className="modal-content-verify">
-        <button
-          className="button-exit-verify"
-          aria-label="Close"
-          type="button"
-          onClick={onClose}
-        >
-          <svg
-            aria-hidden="true"
-            height="16"
-            viewBox="0 0 16 16"
-            version="1.1"
-            width="16"
-          >
-            <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
-          </svg>
-        </button>
         <p className="title-verify">Введите код подтверждения</p>
         <p className="description-verify">
           Код подтверждения был отправлен на вашу электронную почту. Пожалуйста, введите его ниже.
@@ -97,7 +81,7 @@ const VerifyCodeModal = ({ onClose, onSuccess }) => {
           onChange={(e) => setVerificationCode(e.target.value)}
           className="code-input-verify"
         />
-        {error && <p className="error-message">{error}</p>}
+        {error && <p className="error-message-link">{error}</p>}
         <div className="modal-buttons-verify">
           <button className="button-verify red" onClick={handleVerification}>
             Подтвердить
