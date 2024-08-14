f17f67a11396825f89c1b3b3f87f25b0e1e06edd
Ilya Tetyakov <tetyakov2@gmail.com>
2024-08-14 18:57:15 +0700

    Добавил хэдер Base

--- src/components/Global/HeaderLinksPageBase.js
+++ src/components/Global/HeaderLinksPageBase.js
@@ -118,7 +118,7 @@ const HeaderLinksPage = () => {
               className="header-right-status"
               onClick={() => navigator("/price")}
             >
-              Free
+              Base
             </div>
           </div>
           <div
